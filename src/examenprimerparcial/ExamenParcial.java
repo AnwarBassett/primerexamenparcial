79c3534d9ad2d3c30088df7760e9261c2047b130
AnwarBassett <abassettma@gmail.com>
2022-10-08 18:23:28 -0600

    primer metodo agregado

--- src/examenprimerparcial/ExamenParcial.java
+++ src/examenprimerparcial/ExamenParcial.java
@@ -4,35 +4,44 @@ import java.util.Scanner;
 public class ExamenParcial {
 	public static void main(String[] args) {
 		Scanner lec=new Scanner(System.in);
-		int menu, CA, VD, VU, VUU, VUUF;
-		double DA, VL, DPA;
-		System.out.println("Bienvenido al sistema de depreciacion de la UNI");
+		char salida;
+		int menu,  VU;
+		double CA, VD,DA, VL, DPA;
+		System.out.println("Bienvenido al sistema de depreciacion de la UNI"
+				+ "\n ");
+		do {
 		do{System.out.println("Que metodo desea utilizar?  "
 				+ "\n1. Metodo de linea"
 				+ "\n2. Metodo de las unidades producidas"
 				+ "\n3. Metodo de reduccion de saldo"
 				+ "\n4. Metodo de la suma de los digitos por año"); 
+        while(!lec.hasNextInt()){lec.next();System.out.println("Solo numeros puede ingresar");}
 		menu=lec.nextInt();}while(menu<1 || menu>4);
+		
+		do{System.out.print("Por favor, Digite el valor del activo: ");
+        while(!lec.hasNextInt()){lec.next();System.out.println("Solo numeros puede ingresar");}
+		CA=lec.nextInt();}while(CA<1000 || CA>10000000);
+		do{System.out.print("Digite el valor de desecho/residual: ");
+        while(!lec.hasNextInt()){lec.next();System.out.println("Solo numeros puede ingresar");}
+		VD=lec.nextInt();}while(VD<1000 || VD>10000000);
+		do{System.out.print("Digite la vida util del activo: ");
+        while(!lec.hasNextInt()){lec.next();System.out.println("Solo numeros puede ingresar");}
+		VU=lec.nextInt();}while(VU<1 || VU>15);
 		switch(menu) {
+		
 		case 1:
-			do{System.out.println("Por favor, Digite el valor del activo");
-			CA=lec.nextInt();}while(CA<1000 || CA>1000000);
-			do{System.out.println("Digite el valor de desecho/residual");
-			VD=lec.nextInt();}while(VD<1000 || VD>1000000);
-			do{System.out.println("Digite la vida util del activo");
-			VU=lec.nextInt();}while(VU<1 || VU>20);
-			
 			DA=((CA-VD)/VU);
 			VL=CA-DA;
 			DPA=0;
 			System.out.println("Costo del activo: C$"+CA
 					+ "\nValor de Desecho/Residual: C$"+VD+""
-					+ "\nVida Util: C$"+VU+""
-					+ "\nDepresiacion Anual: C$"+DA);
-			
+					+ "\nVida Util: C$"+VU
+					+ "\n ");
+			    System.out.println("Depreciacion Anual | Depreciacion Acumulada | Valor Libros");
+			    System.out.println("----------------------------------------------------------");
 			 for(int i=1;i<=VU;i++) {
 				DPA=DPA+DA;
-				System.out.println(i+". Depreciacion Acumulada="+DPA+" | Valor en libros="+VL);
+				System.out.println(DA+"            | "+DPA+"                | "+VL);
 				VL=VL-DA;
 			 }
 			break;
